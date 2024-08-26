c365d27c24463a7187b1a58b626132447c3fae36
Adrian Skelton <adrianskelton9@gmail.com>
2024-08-26 21:05:50 +0000

    add user app

--- trashsundays/trashsundays/settings.py
+++ trashsundays/trashsundays/settings.py
@@ -27,10 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+LOGIN_REDIRECT_URL = 'profile'
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
