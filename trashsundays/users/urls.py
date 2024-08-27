a5a194df928e4b97ad5abc7511c758bdcaa923f9
Adrian Skelton <adrianskelton9@gmail.com>
2024-08-27 13:27:41 +0000

    migrations made

--- trashsundays/users/urls.py
+++ trashsundays/users/urls.py
@@ -1,10 +1,11 @@
 # users/urls.py
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('accounts/', include('allauth.urls')),
 ]
 
