e2a8962c1040ba7fdbe21b6263fbba1974e228a2
Adrian Skelton <adrianskelton9@gmail.com>
2024-08-28 14:31:49 +0000

    style changes darkmode

--- trashsundays/trashsundays/urls.py
+++ trashsundays/trashsundays/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('', user_views.home, name='home'),  # Home page view
     path('about/', auth_views.LoginView.as_view(template_name='about.html'), name='about'),
     path('contact/', auth_views.LoginView.as_view(template_name='contact.html'), name='contact'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
-    path('register/', user_views.register, name='register'),
+    path('accounts/', include('allauth.urls')),  # allauth URLs
     path('', include('django.contrib.auth.urls')),  # Django's built-in auth URLs
 ]
--- trashsundays/users/views.py
+++ trashsundays/users/views.py
@@ -15,6 +15,20 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to a success page.
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
